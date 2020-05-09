9b27cd23e57dd31534a8ebc06fa4552e0c905e9c
Andy Wigley <anwigley@microsoft.com>
2020-05-09 19:42:08 +0100

    ConfigureAwait added, renaming directories

---
+++ src/Movesensedotnet/TestApp/MovesenseDemo.Android/MainActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android;
+using Plugin.Permissions;
+using Xamarin.Essentials;
+using System.Threading.Tasks;
+
+namespace MovesenseDemo.Droid
+{
+    [Activity(Label = "MovesenseDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        readonly string[] PermissionsLocation =
+        {
+          Manifest.Permission.AccessCoarseLocation
+        };
+        const int RequestLocationId = 0;
+
+        protected override async void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+            Xamarin.Essentials.Platform.Init(this, bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            await CheckAndRequestLocationPermission();
+
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public async Task<PermissionStatus> CheckAndRequestLocationPermission()
+        {
+            var status = await Permissions.CheckStatusAsync<Permissions.LocationWhenInUse>();
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
+            }
+
+            // Additionally could prompt the user to turn on in settings
+
+            return status;
+        }
+    }
+}
+
